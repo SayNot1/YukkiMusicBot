a556e9ff7b727f023baaabdf78a69e8e776bed30
SayNot1 <107950146+SayNot1@users.noreply.github.com>
2022-06-21 21:00:34 +0300

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -57,9 +57,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("TeamYukki")
-                await self.one.join_chat("TheYukki")
-                await self.one.join_chat("YukkiSupport")
+                await self.one.join_chat("TelegramTipsAR")
+                await self.one.join_chat("TelegramTipsAR")
+                await self.one.join_chat("TelegramTipsAR")
             except:
                 pass
             assistants.append(1)
